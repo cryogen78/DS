0ad57b80f3fd636fcc408095f71314e5e2e5f694
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-09-15 20:22:20 +0530

    Update Practical2.py

--- Practical2.py
+++ Practical2.py
@@ -1,6 +1,6 @@
 #Implement Linked List. Include options for insertion, deletion and search of a number, reverse the list and concatenate two linked lists 
 # 3005 Shravan
-class DoubleNode:
+class Node:
     def __init__(self, element, next=None, prev=None):
         self.element = element
         self.next = next
@@ -9,37 +9,23 @@ class DoubleNode:
     def display(self):
         print(self.element)
 
-
-class DoublyLinkedList:
+class LinkedList:
     def __init__(self):
-        self.__head = None
-        self.__tail = None
-        self.__size = 0
+        self.start = None
+        self.end = None
+        self.length = 0
 
     def is_empty(self) -> bool:
-        return self.__size == 0
+        return self.length == 0
 
     def get_size(self) -> int:
-        return self.__size
+        return self.length
 
-    def __display_backward(self):
+    def display(self):
         if self.is_empty():
             print("Doubly Linked List is empty")
             return
-        last = self.__tail
-        print("The List: ", end='')
-        print("[" + last.element, end='')
-        last = last.prev
-        while last:
-            print(", " + last.element, end='')
-            last = last.prev
-        print("]")
-
-    def __display_forward(self):
-        if self.is_empty():
-            print("Doubly Linked List is empty")
-            return
-        first = self.__head
+        first = self.start
         print("The List: ", end='')
         print("[" + first.element, end='')
         first = first.next
@@ -48,112 +34,87 @@ class DoublyLinkedList:
             first = first.next
         print("]")
 
-    def display(self, direction="start"):
-        if direction == "end":
-            self.__display_backward()
-        elif direction == "start":
-            self.__display_forward()
-        else:
-            self.__display_forward()
-
     def add_head(self, e):
-        old_head = self.__head
-        self.__head = DoubleNode(e)
-        self.__head.next = old_head
+        old_head = self.start
+        self.start = Node(e)
+        self.start.next = old_head
         if old_head is not None:
-            old_head.prev = self.__head
-        if self.__tail is None:
-            self.__tail = self.__head
-        self.__size += 1
+            old_head.prev = self.start
+        if self.end is None:
+            self.end = self.start
+        self.length += 1
 
-    def get_head(self) -> DoubleNode:
-        return self.__head
+    def get_head(self) -> Node:
+        return self.start
 
     def remove_head(self):
         if self.is_empty():
             print("Doubly Linked List is empty")
             return
-        elif self.__size == 1:
-            self.__head = None
-            self.__tail = None
+        elif self.length == 1:
+            self.start = None
+            self.end = None
         else:
-            self.__head = self.__head.next
-        self.__size -= 1
+            self.start = self.start.next
+        self.length -= 1
 
     def add_tail(self, e):
-        new_value = DoubleNode(e)
+        new_value = Node(e)
         if self.get_tail() is not None:
-            old_tail = self.__tail
-            self.__tail = new_value
-            self.__tail.prev = old_tail
-            old_tail.next = self.__tail
-            self.__size += 1
+            old_tail = self.end
+            self.end = new_value
+            self.end.prev = old_tail
+            old_tail.next = self.end
+            self.length += 1
         else:
             self.add_head(e)
 
-    def get_tail(self) -> DoubleNode:
-        return self.__tail
+    def get_tail(self) -> Node:
+        return self.end
 
     def remove_tail(self):
         if self.is_empty():
             print("Doubly Linked List is empty")
             return
-        elif self.__size == 1:
-            self.__head = None
-            self.__tail = None
+        elif self.length == 1:
+            self.start = None
+            self.end = None
         else:
-            self.__tail = self.__tail.prev
-        self.__size -= 1
+            self.end = self.end.prev
+        self.length -= 1
 
     def add_between_list(self, index, element):
-        if index > self.__size or index < 0:
+        if index > self.length or index < 0:
             print("Index out of bounds")
-        elif index == self.__size:
+        elif index == self.length:
             self.add_tail(element)
         elif index == 0:
             self.add_head(element)
         else:
             prev_node = self.get_node_at(index - 1)
             current_node = self.get_node_at(index)
-            new_value = DoubleNode(element)
+            new_value = Node(element)
             prev_node.next = new_value
             new_value.next = current_node
             new_value.prev = prev_node
             current_node.prev = new_value
-            self.__size += 1
+            self.length += 1
 
-    def __get_node_from_start(self, index):
-        element_node = self.__head
-        counter = 0
-        while counter < index:
-            element_node = element_node.next
-            counter += 1
-        return element_node
-
-    def __get_node_from_end(self, index):
-        element_node = self.__tail
-        counter = self.__size
-        while counter > index + 1:
-            element_node = element_node.prev
-            counter -= 1
-        return element_node
-
-    def get_node_at(self, index, direction="auto") -> DoubleNode:
-        if index < 0 or index >= self.__size:
+    def get_node_at(self, index, direction="auto") -> Node:
+        if index < 0 or index >= self.length:
             print("Index out of bounds")
-        elif direction == "start":
-            return self.__get_node_from_start(index)
-        elif direction == "end":
-            return self.__get_node_from_end(index)
-        elif self.__size == 1 or index <= self.__size // 2:
-            return self.__get_node_from_start(index)
-        elif index > self.__size // 2:
-            return self.__get_node_from_end(index)
+        else:
+            element_node = self.start
+            counter = 0
+            while counter < index:
+                element_node = element_node.next
+                counter += 1
+            return element_node
 
     def remove_between_list(self, index):
-        if index < 0 or index >= self.__size:
+        if index < 0 or index >= self.length:
             print("Index out of bounds")
-        elif index == self.__size - 1:
+        elif index == self.length - 1:
             self.remove_tail()
         elif index == 0:
             self.remove_head()
@@ -162,60 +123,49 @@ class DoublyLinkedList:
             next_node = self.get_node_at(index + 1)
             prev_node.next = next_node
             next_node.prev = prev_node
-            self.__size -= 1
-
-    def search(self, search_value) -> bool:
-        index = 0
-        while index < self.__size:
-            value = self.get_node_at(index)
-            if value.element == search_value:
-                print("Found value '" + str(search_value) + "' at location: " + str(index))
-                return True
-            index += 1
-        print("Couldn't find value '" + str(search_value) + "'")
-        return False
+            self.length -= 1
 
     def merge(self, linked_list_value):
         if not self.is_empty():
             last_node = self.get_tail()
-            last_node.next = linked_list_value.__head
+            last_node.next = linked_list_value.start
             if not linked_list_value.is_empty():
-                linked_list_value.__head.prev = last_node
-                self.__tail = linked_list_value.__tail
-            self.__size = self.__size + linked_list_value.__size
+                linked_list_value.start.prev = last_node
+                self.end = linked_list_value.end
+            self.length = self.length + linked_list_value.length
         else:
-            self.__head = linked_list_value.__head
-            self.__tail = linked_list_value.__tail
-            self.__size = linked_list_value.__size
+            self.start = linked_list_value.start
+            self.end = linked_list_value.end
+            self.length = linked_list_value.length
 
     def reverse(self):
         if self.is_empty():
             print("Doubly Linked List is empty")
             return
-        elif self.__size == 1:
+        elif self.length == 1:
             return
-        elif self.__size == 2:
-            temp = self.__head
-            self.__head = self.__tail
-            self.__tail = temp
-            self.__head.prev = None
-            self.__head.next = self.__tail
-            self.__tail.prev = self.__head
-            self.__tail.next = None
-        elif self.__size == 3:
+        elif self.length == 2:
+            temp = self.start
+            self.start = self.end
+            self.end = temp
+            self.start.prev = None
+            self.start.next = self.end
+            self.end.prev = self.start
+            self.end.next = None
+        elif self.length == 3:
             mid = self.__get_node_from_start(1)
-            temp = self.__head
-            self.__head = self.__tail
-            self.__tail = temp
-            self.__head.prev = None
-            self.__head.next = mid
-            mid.prev = self.__head
-            mid.next = self.__tail
-            self.__tail.prev = mid
-            self.__tail.next = None
-        elif self.__size > 3:
+            temp = self.start
+            self.start = self.end
+            self.end = temp
+            self.start.prev = None
+            self.start.next = mid
+            mid.prev = self.start
+            mid.next = self.end
+            self.end.prev = mid
+            self.end.next = None
+        elif self.length > 3:
             temp_lst = []
-            first = self.__head
+            first = self.start
             temp_lst.append(first)
             first = first.next
             while first:
@@ -224,97 +174,83 @@ class DoublyLinkedList:
             temp_lst.reverse()
             for i in range(0, len(temp_lst)):
                 if i == 0:
-                    self.__head = temp_lst[i]
-                    self.__head.prev = None
-                    self.__head.next = temp_lst[i+1]
+                    self.start = temp_lst[i]
+                    self.start.prev = None
+                    self.start.next = temp_lst[i + 1]
                 elif i == 1:
-                    temp_lst[i].prev = self.__head
-                    temp_lst[i].next = temp_lst[i+1]
-                elif i == len(temp_lst)-1:
-                    self.__tail = temp_lst[i]
-                    self.__tail.prev = temp_lst[i-1]
-                    self.__tail.next = None
+                    temp_lst[i].prev = self.start
+                    temp_lst[i].next = temp_lst[i + 1]
+                elif i == len(temp_lst) - 1:
+                    self.end = temp_lst[i]
+                    self.end.prev = temp_lst[i - 1]
+                    self.end.next = None
                 else:
-                    temp_lst[i].prev = temp_lst[i-1]
-                    temp_lst[i].next = temp_lst[i+1]
-            temp_lst[len(temp_lst)-2].prev = temp_lst[len(temp_lst)-2-1]
-            temp_lst[len(temp_lst)-2].next = self.__tail
-
-    @staticmethod
-    def test_main():
-        dll = DoublyLinkedList()
-        print(dll.is_empty())
-        dll.add_between_list(0, "zero")
-        dll.add_between_list(1, "one")
-        dll.add_between_list(2, "two")
-        dll.display()
-        print(dll.is_empty())
-        print("After removing")
-        dll.remove_between_list(1)
-        dll.display()
-        print("After adding again")
-        dll.add_between_list(1, "one")
-        dll.display()
-
-        dll2 = DoublyLinkedList()
-        dll2.add_head("first_name")
-        dll2.add_tail("last_name")
-        print("Head: ", end='')
-        dll2.get_head().display()
-        print("Tail: ", end='')
-        dll2.get_tail().display()
-        dll2.remove_tail()
-        dll2.get_tail().display()
-        dll2.remove_head()
-        try:
-            dll2.get_head().display()
-        except AttributeError as e:
-            print(e, "\nNo head found")
-        dll2.display()
-
-        dll3 = DoublyLinkedList()
-        dll3.add_between_list(0, "zero")
-        dll3.add_between_list(1, "one")
-        dll3.add_between_list(2, "two")
-        dll3.add_between_list(3, "three")
-        dll3.display()
-        dll3.search("two")
-        dll3.search("four")
-        dll3.get_node_at(3).display()
-        dll3.get_node_at(3, "start").display()
-        dll3.get_node_at(3, "end").display()
-
-        dll4 = DoublyLinkedList()
-        dll4.add_between_list(0, "0")
-        dll4.add_between_list(1, "1")
-        dll4.add_between_list(2, "2")
-        dll4.add_between_list(3, "4")
-        print("The Two Lists: ")
-        dll3.display()
-        print("Head: ", end='')
-        dll3.get_head().display()
-        print("Tail: ", end='')
-        dll3.get_tail().display()
-        dll4.display()
-        print("Head: ", end='')
-        dll4.get_head().display()
-        print("Tail: ", end='')
-        dll4.get_tail().display()
-        dll3.merge(dll4)
-        print("After Merging")
-        dll3.display()
-        print("Head: ", end='')
-        dll3.get_head().display()
-        print("Tail: ", end='')
-        dll3.get_tail().display()
-        print(f"Size: {dll3.get_size()}")
-        dll3.display()
-        dll3.reverse()
-        print("After Reversing")
-        dll3.display()
-        print("Reverse Display")
-        dll3.display("end")
+                    temp_lst[i].prev = temp_lst[i - 1]
+                    temp_lst[i].next = temp_lst[i + 1]
+            temp_lst[len(temp_lst) - 2].prev = temp_lst[len(temp_lst) - 2 - 1]
+            temp_lst[len(temp_lst) - 2].next = self.end
 
 
-if __name__ == "__main__":
-    DoublyLinkedList.test_main()
+one = LinkedList()
+print(one.is_empty())
+one.add_between_list(0, "zero")
+one.add_between_list(1, "one")
+one.add_between_list(2, "two")
+one.display()
+print(one.is_empty())
+one.remove_between_list(1)
+one.display()
+one.add_between_list(1, "one")
+one.display()
+
+two = LinkedList()
+two.add_head("first_name")
+two.add_tail("last_name")
+print("Head: ", end='')
+two.get_head().display()
+print("Tail: ", end='')
+two.get_tail().display()
+two.remove_tail()
+two.get_tail().display()
+two.remove_head()
+try:
+    two.get_head().display()
+except AttributeError as e:
+    print(e, "\nNo head found")
+two.display()
+
+two = LinkedList()
+two.add_between_list(0, "zero")
+two.add_between_list(1, "one")
+two.add_between_list(2, "two")
+two.add_between_list(3, "three")
+two.display()
+two.get_node_at(3).display()
+two.get_node_at(3, "start").display()
+two.get_node_at(3, "end").display()
+
+three = LinkedList()
+three.add_between_list(0, "0")
+three.add_between_list(1, "1")
+three.add_between_list(2, "2")
+three.add_between_list(3, "4")
+two.display()
+print("Head: ", end='')
+two.get_head().display()
+print("Tail: ", end='')
+two.get_tail().display()
+three.display()
+print("Head: ", end='')
+three.get_head().display()
+print("Tail: ", end='')
+three.get_tail().display()
+two.merge(three)
+two.display()
+print("Head: ", end='')
+two.get_head().display()
+print("Tail: ", end='')
+two.get_tail().display()
+print(f"Size: {two.get_size()}")
+two.display()
+two.reverse()
+two.display()
