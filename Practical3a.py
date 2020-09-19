92412216a98ec702d9a9935e471c22fe57ec6d20
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-09-19 18:01:08 +0530

    Update Practical3a.py
    To create a Stack For implementation of Array.

--- Practical3a.py
+++ Practical3a.py
@@ -1,1 +1,29 @@
 #3005 Shravan
+#To create a Stack For implementation of Array.
+class Stack_5:
+     def _init_(self):
+         self.container = []  
+
+     def isEmpty(self):
+         return self.size() == 0   
+
+     def push(self, item):
+         self.container.append(item)  
+
+     def pop(self):
+         return self.container.pop()    
+
+     def size(self):
+         return len(self.container)  
+
+     def show(self):
+         return self.container 
+
+s = Stack_5()
+s.push('1')
+s.push('2')
+s.push('5')
+s.push('3')
+s.push('4')
+print(s.pop())
+print(s.show())
