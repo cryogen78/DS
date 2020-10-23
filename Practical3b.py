3f4fe8bc5b983baefa653fad5b4e7e1e26415767
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-10-23 18:16:58 +0530

    Update Practical3b.py

--- Practical3b.py
+++ Practical3b.py
@@ -1,2 +1,55 @@
 #3005 (New Rollno:372) Shravan SYIT
 #Implementation of the Tower Of Hanoi.
+class Stack:
+
+    def __init__(self):
+        self.stack_arr = []
+
+    def push(self,value):
+        self.stack_arr.append(value)
+
+    def pop(self):
+        if len(self.stack_arr) == 0:
+            print('Stack is empty!')
+            return None
+        else:
+            self.stack_arr.pop()
+
+    def get_head(self):
+        if len(self.stack_arr) == 0:
+            print('Stack is empty!')
+            return None
+        else:
+            return self.stack_arr[-1]
+
+    def display(self):
+        if len(self.stack_arr) == 0:
+            print('Stack is empty!')
+            return None
+        else:
+            print(self.stack_arr)
+
+A = Stack()
+B = Stack()
+C = Stack()
+def Hanoi(n, fromrod,to,temp):
+    if n == 1:
+        fromrod.pop()
+        to.push('disk 1')
+        if to.display() != None:
+            print(to.display())
+
+    else:
+
+        Hanoi(n-1, fromrod, temp, to)
+        fromrod.pop()
+        to.push(f'disk {n}')
+        if to.display() !=  None:
+            print(to.display())
+        Hanoi(n-1, temp, to, fromrod)
+
+n = int(input('Enter the number of the disk in rod A : '))
+for i in  range(n):
+    A.push(f'disk {i+1} ')
+
+Hanoi(n, A, C, B)
