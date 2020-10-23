2c7eea1666b286ce254c593c220ce0d54525bace
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-10-23 13:51:54 +0530

    Create Practical7a.py

---
+++ Practical7a.py
@@ -0,0 +1,28 @@
+#3005 (New Rollno:372) Shravan SYIT
+#Implement the following Hashing Techniques. 
+#Write a program to implement the collision technique.
+
+class Hash:
+    def _init_(self, keys, lowerrange, higherrange):
+        self.value = self.hashfunction(keys,lowerrange, higherrange)
+
+    def get_key_value(self):
+        return self.value
+
+    def hashfunction(self,keys,lowerrange, higherrange):
+        if lowerrange == 0 and higherrange > 0:
+            return keys%(higherrange)
+
+if _name_ == '_main_':
+    list_of_keys = [23,43,1,87]
+    list_of_list_index = [None,None,None,None]
+    print("Before : " + str(list_of_list_index))
+    for value in list_of_keys:
+        #print(Hash(value,0,len(list_of_keys)).get_key_value())
+        list_index = Hash(value,0,len(list_of_keys)).get_key_value()
+        if list_of_list_index[list_index]:
+            print("Collission detected")
+        else:
+            list_of_list_index[list_index] = value
+
+    print("After: " + str(list_of_list_index))
