142a9c08938c6b476f2a41ced851a49eee69052e
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-10-24 20:35:12 +0530

    Update Practical6.py

--- Practical6.py
+++ Practical6.py
@@ -1,43 +1,42 @@
 #to sort a list of elements. Give user the option to perform sorting using Insertion sort, Bubble sort or Selection sort.
 #3005 (New Rollno:372) Shravan SYIT
 class Sort:
-    def __init__(self, list):
-        self.list = list
+    def __init__(self, lst):
+        self.lst = lst
 
     def bubble_sort(self):
-        sorted_list = self.list.copy()
-        for i in range(len(sorted_list) - 1):
-            for j in range(0, len(sorted_list) - i - 1):
-                if sorted_list[j] > sorted_list[j + 1]:
-                    sorted_list[j], sorted_list[j + 1] = sorted_list[j + 1], sorted_list[j]
-        return sorted_list
+        sorted_lst = self.lst.copy()
+        for i in range(len(sorted_lst) - 1):
+            for j in range(0, len(sorted_lst) - i - 1):
+                if sorted_lst[j] > sorted_lst[j + 1]:
+                    sorted_lst[j], sorted_lst[j + 1] = sorted_lst[j + 1], sorted_lst[j]
+        return sorted_lst
 
     def selection_sort(self):
-        sorted_list = self.list.copy()
-        for i in range(len(sorted_list)):
-            min_index = i
+        sorted_lst = self.lst.copy()
+        for i in range(len(sorted_lst)):
+            min_idx = i
             for j in range(i + 1, len(sorted_lst)):
-                if sorted_list[min_index] > sorted_list[j]:
-                    min_index = j
-            sorted_list[i], sorted_list[min_index] = sorted_list[min_index], sorted_list[i]
-        return sorted_list
+                if sorted_lst[min_idx] > sorted_lst[j]:
+                    min_idx = j
+            sorted_lst[i], sorted_lst[min_idx] = sorted_lst[min_idx], sorted_lst[i]
+        return sorted_lst
 
     def insertion_sort(self):
-        sorted_list = self.list.copy()
-        for i in range(1, len(sorted_list)):
-            key = sorted_list[i]
+        sorted_lst = self.lst.copy()
+        for i in range(1, len(sorted_lst)):
+            key = sorted_lst[i]
             j = i - 1
-            while j >= 0 and key < sorted_list[j]:
-                sorted_list[j + 1] = sorted_list[j]
+            while j >= 0 and key < sorted_lst[j]:
+                sorted_lst[j + 1] = sorted_lst[j]
                 j -= 1
-            sorted_list[j + 1] = key
-        return sorted_list
+            sorted_lst[j + 1] = key
+        return sorted_lst
 
 
-if __name__ == '__rollno__':
-    test_list = [31,44,108,109,16,94,5]
+if __name__ == '__main__':
+    test_list = [44,31,108,94,109]
     test_sort = Sort(test_list)
-    print(test_sort.bubble_sort(test_sort))
+    print(test_sort.bubble_sort())
     print(test_sort.selection_sort())
     print(test_sort.insertion_sort())
-
