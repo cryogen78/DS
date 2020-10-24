4c515432105d73fe2ad44005b2badb8e86505150
cryogen78 <52559503+cryogen78@users.noreply.github.com>
2020-10-24 20:28:45 +0530

    Update Practical5.py

--- Practical5.py
+++ Practical5.py
@@ -1,39 +1,38 @@
 #to search an element from a list and give user the option to perform Linear or Binary search. 
 #3005 (New Rollno:372) Shravan SYIT
-class Search :
-    def __init__ ( self , list , element ) :
-        self.list = list
-        self.element = element
+class Search:
+    def __init__(self, lst, ele):
+        self.lst = lst
+        self.ele = ele
 
-    def binary_search ( self ) :
-        sorted_list = sorted ( self.list )
+    def binary_search(self):
+        sorted_lst = sorted(self.lst)
         start = 0
-        end = len ( sorted_list ) - 1
-        while start <= end :
+        end = len(sorted_lst) - 1
+        while start <= end:
             mid = (end + start) // 2
-            if sorted_list [ mid ] < self.element :
+            if sorted_lst[mid] < self.ele:
                 start = mid + 1
-            elif sorted_list [ mid ] > self.element :
+            elif sorted_lst[mid] > self.ele:
                 end = mid - 1
-            else :
+            else:
                 return mid
         return False
 
-    def linear_search ( self ) :
-        for i in range ( len ( self.list ) ) :
-            if self.list [ i ] == self.element :
+    def linear_search(self):
+        for i in range(len(self.lst)):
+            if self.lst[i] == self.ele:
                 return i
         return False
 
 
-if __name__ == '__rollno__' :
-    test_list = [ 31,94,44,108,5,16 ]
-    testvalue_1 = 94
-    testvalue_2 = 44
-    test_search = Search ( test_list , testvalue_1 )
-    print ( test_search.binary_search ( ) )
-    print ( test_search.linear_search ( ) )
-    testsearch_2 = Search ( test_list , testvalue_2 )
-    print ( testsearch_2.binary_search ( ) )
-    print ( testsearch_2.linear_search ( ) )
-
+if __name__ == '__main__':
+    test_list = [5,31,44,94,108,109]
+    test_value_1 = 42
+    test_value_2 = 44
+    test_search = Search(test_list, test_value_1)
+    print(test_search.binary_search())
+    print(test_search.linear_search())
+    test_search_2 = Search(test_list, test_value_2)
+    print(test_search_2.binary_search())
+    print(test_search_2.linear_search())
